5f1a45a306d547f2cbd1c486b3afba7b4cf99b99
TeoMunoz <teo.munoz.blasco@gmail.com>
2023-02-27 16:16:27 +0100

    log in

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+var people = [
+    {
+        username: "user",
+        password: "password1234"
+    }
+]
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for( i = 0; i < people.length; i++) {
+        if(username == people[i].username && password == people[i].password) {
+            console.log(username + " is logged in!")
+        }else {
+            console.log("incorrect username or password")
+        }
+    }
+}
